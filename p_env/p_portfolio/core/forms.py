27394d7ff120a9e0a15c9ca0611db2273121db4b
shakib <s@yahoo.com>
2020-05-29 18:20:26 +0600

    form updated

--- p_env/p_portfolio/core/forms.py
+++ p_env/p_portfolio/core/forms.py
@@ -4,4 +4,19 @@ from .models import Contact
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ('name', 'email', 'phone', 'service', 'comment')
+        labels  = {
+        'name':'Your name', 
+        'email':'Your email', 
+        'phone':'Your phone', 
+        'service':'Choose the service you wanna talk about', 
+        'comment':'Write your message'
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'service': forms.Select(attrs={'class': 'form-control'}),
+            'comment': forms.Textarea(
+                attrs={'class': 'form-control'}),
+        }
+        fields = ['name', 'email', 'phone', 'service', 'comment']
