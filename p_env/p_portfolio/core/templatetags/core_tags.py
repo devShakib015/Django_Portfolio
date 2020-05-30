9694301ccac15c5827006688f1ff3e0ecfcd3ac0
shakib <s@yahoo.com>
2020-05-31 02:24:36 +0600

    Designed

--- p_env/p_portfolio/core/templatetags/core_tags.py
+++ p_env/p_portfolio/core/templatetags/core_tags.py
@@ -8,3 +8,21 @@ def testimonial():
     testimonials = Testimonial.objects.all()
 
     return{'testimonials': testimonials}
+
+@register.inclusion_tag('video_tag.html')
+def video_tag(count=3):
+    videos = Video.objects.order_by('-id')[0:3]
+
+    return{'videos': videos}
+
+@register.inclusion_tag('service_tag.html')
+def service_tag(count=3):
+    services = Service.objects.order_by('-id')[0:3]
+
+    return{'services': services}
+
+@register.inclusion_tag('work_tag.html')
+def work_tag(count=3):
+    works = RecentWorks.objects.order_by('-id')[0:3]
+
+    return{'works': works}
